4690bd8ab8cd1187a560fca2ed6600f1a3152211
echofreedom007 <b.zhang41@outlook.com>
2024-01-05 21:28:48 -0500

    fix: tweak hooks

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import Input from "../../shared/components/FormElements/Input";
@@ -43,30 +43,49 @@ const DUMMY_PLACES = [
 ];
 
 const UpdatePlace = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
   const placeId = useParams().placeId;
+
+  const [formState, inputHandler, setFormData] = useForm(
+    {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+    },
+    false
+  );
 
   const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
 
-  const [formState, inputHandler] = useForm(
-    {
-      title: {
-        value: identifiedPlace.title,
-        isValid: true,
+  useEffect(() => {
+    setFormData(
+      {
+        title: {
+          value: identifiedPlace.title,
+          isValid: true,
+        },
+        description: {
+          value: identifiedPlace.description,
+          isValid: true,
+        },
       },
-      description: {
-        value: identifiedPlace.description,
-        isValid: true,
-      },
-    },
-    true
-  );
+      true
+    );
+    setIsLoading(false);
+  }, [setFormData, identifiedPlace]);
 
   const placeUpdateSubmitHandler = (e) => {
     e.preventDefault();
     console.log(formState.inputs);
   };
 
-  if (!identifiedPlace.length === 0) {
+  if (!identifiedPlace) {
     return (
       <div className="center">
         <h2>Could not find place!</h2>
@@ -74,6 +93,15 @@ const UpdatePlace = () => {
     );
   }
 
+  //it only loads the data once to empty data, to set the from using fetched data from
+  // backend, we need to set the form ready only when the data is ready.
+  if (isLoading) {
+    return (
+      <div className="center">
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
   return (
     <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
       <Input
